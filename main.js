37cb378f3884e486ed1192347bd0ab35140949b4
apta-tamilarasi <athinarayanantamilarasi@gamil.com>
2023-09-09 14:26:03 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,14 @@
+let a=parseInt(prompt("Enter the 1st number"))
+let b=parseInt(prompt("Enter the 2st number"))
+
+document.write("a = "+a+"<br>")
+document.write("b = "+b+"<br>")
+
+if(a==b){
+	document.write("Given number is equal")
+}
+
+else if(a!=b){
+	document.write("Given number is not equal")
+	
+}
